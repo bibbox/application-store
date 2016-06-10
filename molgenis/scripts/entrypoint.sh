dd899e0c2cc4874acabc3b9f5e3b20b9ad4d45a6
reihsr <robert.reihs@gmail.com>
2016-06-10 15:03:59 +0200

    Update Molgenis Docker

--- molgenis/scripts/entrypoint.sh
+++ molgenis/scripts/entrypoint.sh
@@ -3,13 +3,17 @@ file="/opt/molgenis/molgenis-server.properties"
 
 if [[ ! -f "$file" ]]; then
 
-echo << 'EOF'
+cat << EOF > $file
 admin.password=${MOLGENIS_ADMIN_PASSWORD}
 admin.email=${MOLGENIS_ADMIN_email}
 db_user=${MYSQL_USER}
 db_password=${MYSQL_PASSWORD}
 db_uri=jdbc\:mysql\://${MOLGENIS-MYSQL_PORT_5432_TCP_ADDR}/${MYSQL_DATABASE}
 EOF
+
+
+
+fi
 
 $CATALINA_HOME/bin/startup.sh
 tail -f $CATALINA_HOME/logs/catalina.out
