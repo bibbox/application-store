395083a74b545cf5a741a2dbdd07fb1ebe3e2274
reihsr <robert.reihs@gmail.com>
2016-07-06 21:11:11 +0200

    Molgenis Docker entry point update

--- molgenis/scripts/entrypoint.sh
+++ molgenis/scripts/entrypoint.sh
@@ -6,9 +6,9 @@ if [[ ! -f "$file" ]]; then
 cat << EOF > $file
 admin.password=${MOLGENIS_ADMIN_PASSWORD}
 admin.email=${MOLGENIS_ADMIN_email}
-db_user=MYSQL_USER
+db_user=${MYSQL_USER}
 db_password=${MYSQL_PASSWORD}
-db_uri=jdbc\:mysql\://molgenisdb/MYSQL_DATABASE
+db_uri=jdbc\:mysql\://molgenisdb/${MYSQL_DATABASE}
 molgenis.version=31
 EOF
 
