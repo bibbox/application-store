265504cf42e28e5aad4e4416b42ef89db54633f6
reihsr <robert.reihs@gmail.com>
2016-07-14 10:08:54 +0200

    update irods config

--- irods/images/irods-cloudbrowser/scripts/entrypoint.sh
+++ irods/images/irods-cloudbrowser/scripts/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ ! -f "$file" ]]; then
 /opt/script/addproxy.sh -t irods-cloud-backend -s ${CLOUD_BACKEND_SUBDOMAIN} -p ${CLOUD_BACKEND_PORT} -u ${CLOUD_BACKEND_URL} -i ${CLOUD_BACKEND_IP}
 sed -i 's/location.hostname+":8080/${CLOUD_BACKEND_SUBDOMAIN}.${CLOUD_BACKEND_URL}/g' /var/www/html/app/components/
 service apache2 restart
-sed -i "s/beconf.login.preset.host='localhost'/beconf.login.preset.host='${IRODS_HOST_URL}'" /opt/install/irods-cloud-backend-config.groovy
+sed -i "s/beconf.login.preset.host='localhost'/beconf.login.preset.host='irods-icat'" /opt/install/irods-cloud-backend-config.groovy
 mv /opt/install/irods-cloud-backend-config.groovy /etc/irods-cloud-backend-config.groovy
 fi 
 
