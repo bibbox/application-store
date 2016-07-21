060167c8564e1c5b5145eae3ba62164ce2484fe7
reihsr <robert.reihs@gmail.com>
2016-07-21 11:11:04 +0200

    Liferay Updates Compose and Images

--- liferay/image/liferay-application/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Update Config Files
-
-service liferay start
-tail -f /opt/liferay/tomcat-7.0.62/logs/catalina.out
---
+++ liferay/image/liferay-application/scripts/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Starting Liferay Application Container!"
+
+file="/opt/liferay/portal-setup-wizard.properties"
+tomcat="tomcat-7.0.62"
+
+#Config
+if [[ ! -f "$file" ]]; then
+
+ln /conf/portal-ext.properties /opt/liferay/portal-ext.properties
+ln /conf/functions /ect/init.d/functions
+
+sed -i 's/DATABASEPASSWORD/${POSTGRES_PASSWORD}/g' /conf/portal-setup-wizard.properties
+ln /conf/portal-setup-wizard.properties /opt/liferay/portal-setup-wizard.properties
+
+sed -i 's/TOMCAT_INSTANCE_VERSION_NUMBER/$tomcat/g' /conf/liferay
+ln /conf/liferay /etc/init.d/liferay
+
+chown -r liferay:liferay /opt/liferay
+
+fi
+
+#Startu Up
+service liferay start
+tail -f /opt/liferay/$tomcat/logs/catalina.out
