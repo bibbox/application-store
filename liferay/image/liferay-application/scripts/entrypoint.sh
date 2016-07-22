b6f514005ef15438f870236a14d3a03499773bfd
reihsr <robert.reihs@gmail.com>
2016-07-22 11:37:17 +0200

    Update Liferay Containers

--- liferay/image/liferay-application/scripts/entrypoint.sh
+++ liferay/image/liferay-application/scripts/entrypoint.sh
@@ -10,10 +10,10 @@ if [[ ! -f "$file" ]]; then
 ln /conf/portal-ext.properties /opt/liferay/portal-ext.properties
 ln /conf/functions /ect/init.d/functions
 
-sed -i 's/DATABASEPASSWORD/${POSTGRES_PASSWORD}/g' /conf/portal-setup-wizard.properties
+sed -i "s/DATABASEPASSWORD/${POSTGRES_PASSWORD}/g" /conf/portal-setup-wizard.properties
 ln /conf/portal-setup-wizard.properties /opt/liferay/portal-setup-wizard.properties
 
-sed -i 's/TOMCAT_INSTANCE_VERSION_NUMBER/$tomcat/g' /conf/liferay
+sed -i "s/TOMCAT_INSTANCE_VERSION_NUMBER/$tomcat/g" /conf/liferay
 ln /conf/liferay /etc/init.d/liferay
 
 chown -r liferay:liferay /opt/liferay
