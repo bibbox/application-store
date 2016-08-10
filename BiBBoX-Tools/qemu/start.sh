b3afa769f11846e346d2663e376ab7f3f2b5c2bd
reihsr <robert.reihs@gmail.com>
2016-08-10 21:35:01 +0200

    Update openspecimen

--- BiBBoX-Tools/qemu/start.sh
+++ BiBBoX-Tools/qemu/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t bibbox/qemu .
 
-docker run -t -i --name convertdisk -v /Users/reihsr/Downloads/wp5.debian.qcow2:/opt/image/wp5.qcow2 -v /Volumes/Backup/imagesconverted:/opt/image/output bibbox/qemu /bin/bash
+docker run -t -i --name convertdisk -v /Users/reihsr/Downloads/wp5.debian.qcow2:/opt/image/wp5.qcow2 -v /Users/reihsr/Documents/BiBBox/wp5.vdi:/opt/image/wp5.vdi bibbox/qemu /bin/bash
 
 docker rm convertdisk
