055798b7d29eb186f2de2f8e32183b9a5318a0e7
reihsr <robert.reihs@gmail.com>
2016-08-15 21:14:40 +0200

    Fix Bug Liferay Application

--- liferay/image/liferay-application/scripts/entrypoint.sh
+++ liferay/image/liferay-application/scripts/entrypoint.sh
@@ -11,10 +11,12 @@ cat << EOF > $file
 Liferay 0.0.1 installed
 EOF
 
+cp /opt/config/liferay /etc/init.d/liferay
+cp /opt/config/portal-setup-wizard.properties /opt/liferay/portal-setup-wizard.properties
 sed -i "s/DATABASEPASSWORD/${POSTGRES_PASSWORD}/g" /opt/liferay/portal-setup-wizard.properties
 sed -i "s/TOMCAT_INSTANCE_VERSION_NUMBER/$tomcat/g" /etc/init.d/liferay
 
-chown -r liferay:liferay /opt/liferay
+chown -R liferay:liferay /opt/liferay
 chmod +x /etc/init.d/functions
 chmod +x /etc/init.d/liferay
 
