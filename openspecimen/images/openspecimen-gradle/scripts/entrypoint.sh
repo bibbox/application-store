df116285c8358e322d2d4b8ab21a02bb2276b2bb
reihsr <robert.reihs@gmail.com>
2016-09-19 14:59:11 +0200

    Open Specimen Config Update for auto build

--- openspecimen/images/openspecimen-gradle/scripts/entrypoint.sh
+++ openspecimen/images/openspecimen-gradle/scripts/entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 echo "Starting Gradle Container for OpenSpecimen!"
 
-wget -O openspec.zip "$releaseURL"
+wget -O openspec.zip "$RELEASEURL"
+unzip openspec.zip
 mv openspecimen* openspecimen
 cd /opt/openspecimen/
+
+sed -i "s/<app_container_home>/${TOMCATPATH}/g" /opt/openspecimen/build.properties
+sed -i "s/<app_data_dir>/???/g" /opt/openspecimen/build.properties
+sed -i "s/app_log_conf =/app_log_conf =???/g" /opt/openspecimen/build.properties
+#datasource_jndi = java:/comp/env/jdbc/openspecimen
+#deployment_type = fresh
+sed -i "s/database_host = localhost/database_host = openspecimen-db-${INSTANCE}/g" /opt/openspecimen/build.properties
+sed -i "s/database_name = osdb/database_name = ${MYSQL_DATABASE}/g" /opt/openspecimen/build.properties
+sed -i "s/database_username = root/database_username = ${MYSQL_USER}/g" /opt/openspecimen/build.properties
+sed -i "s/database_password = root/database_password = ${MYSQL_PASSWORD}/g" /opt/openspecimen/build.properties
+#show_sql = false
+#plugin_dir = <path-to-directory-containing-plugin-jars>
+
 npm install -g grunt
 gradle buildNeeded
 
@@ -15,4 +29,4 @@ gradle buildNeeded
 
 #Edit : Updated solution for new versions:
 
-# npm install grunt --save-dev
+#rm  npm install grunt --save-dev
