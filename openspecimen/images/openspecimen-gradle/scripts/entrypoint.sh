539733d93800e727bc8410c525a801d27fab37ec
reihsr <robert.reihs@gmail.com>
2016-09-19 15:08:50 +0200

    Added Config Parameters for the openspecimen build

--- openspecimen/images/openspecimen-gradle/scripts/entrypoint.sh
+++ openspecimen/images/openspecimen-gradle/scripts/entrypoint.sh
@@ -6,17 +6,13 @@ unzip openspec.zip
 mv openspecimen* openspecimen
 cd /opt/openspecimen/
 
-sed -i "s/<app_container_home>/${TOMCATPATH}/g" /opt/openspecimen/build.properties
-sed -i "s/<app_data_dir>/???/g" /opt/openspecimen/build.properties
-sed -i "s/app_log_conf =/app_log_conf =???/g" /opt/openspecimen/build.properties
-#datasource_jndi = java:/comp/env/jdbc/openspecimen
-#deployment_type = fresh
+sed -i "s#<app_container_home>#/opt/tomcat#g" /opt/openspecimen/build.properties
+sed -i "s#<app_data_dir>#/opt/openspecimen/os-data#g" /opt/openspecimen/build.properties
 sed -i "s/database_host = localhost/database_host = openspecimen-db-${INSTANCE}/g" /opt/openspecimen/build.properties
 sed -i "s/database_name = osdb/database_name = ${MYSQL_DATABASE}/g" /opt/openspecimen/build.properties
 sed -i "s/database_username = root/database_username = ${MYSQL_USER}/g" /opt/openspecimen/build.properties
 sed -i "s/database_password = root/database_password = ${MYSQL_PASSWORD}/g" /opt/openspecimen/build.properties
-#show_sql = false
-#plugin_dir = <path-to-directory-containing-plugin-jars>
+sed -i "s#<path-to-directory-containing-plugin-jars>#/opt/openspecimen/os-plugins#g" /opt/openspecimen/build.properties
 
 npm install -g grunt
 gradle buildNeeded
