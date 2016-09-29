5d3ef2a1d40273d96db62f06f743b395f54769cb
Sebastian Humpel <sebastian.humpel@student.tugraz.at>
2016-09-29 19:23:16 +0200

    added placeholder to compose file

--- labkey/image/labkey/scripts/entrypoint.sh
+++ labkey/image/labkey/scripts/entrypoint.sh
@@ -11,7 +11,7 @@ LabKey installed
 EOF
 
 sed -i "s:@@appDocBase@@:$appDocBase:g" /usr/local/tomcat/conf/Catalina/localhost/labkey.xml
-sed -i "s:@@jdbcUser@@:${POSTGRES_USER}:g" /usr/local/tomcat/conf/Catalina/localhost/labkey.xml
+sed -i "s:@@jdbcUser@@:labkey:g" /usr/local/tomcat/conf/Catalina/localhost/labkey.xml
 sed -i "s:@@jdbcPassword@@:${POSTGRES_PASSWORD}:g" /usr/local/tomcat/conf/Catalina/localhost/labkey.xml
 
 sed -i "s:@@smtpHost@@:${SMTP_HOST}:g" /usr/local/tomcat/conf/Catalina/localhost/labkey.xml
